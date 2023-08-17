a5878cda7dd364a289475a6f1fb8d1977ebeb487
Jack Delaney <jdelaney07@qub.ac.uk>
2023-08-17 19:35:09 +0100

    adding custom choice for SpeakButton Colours

--- Sources/SpeakStyle/SpeakButton.swift
+++ Sources/SpeakStyle/SpeakButton.swift
@@ -20,17 +20,22 @@ import SwiftUI
 
 public struct SpeakButton: View {
     private var text : String
-    
+    private var colour : Color
     
     /// Creates a SpeakButton view with the desired text for display in button and on tap through text to speech.
     ///
     /// - Parameter text : The text you want displayed in the SpeakButton and spoken to the user upon their tapping on the SpeakButton
     public init(text : String) {
         self.text = text
+        self.colour = Color.accentColor
+    }
+    public init(text : String,colour : Color) {
+        self.text = text
+        self.colour = colour
     }
 
     public var body: some View {
-        Label(text, systemImage: "speaker.1.fill").labelStyle(.titleAndIcon).frame(maxHeight: 1).padding().foregroundColor(.white).background(Color.blue).cornerRadius(20).speakable(withText: text)
+        Label(text, systemImage: "speaker.1.fill").labelStyle(.titleAndIcon).frame(maxHeight: 1).padding().foregroundColor(.white).background(colour).cornerRadius(20).speakable(withText: text)
 
     }
 }
